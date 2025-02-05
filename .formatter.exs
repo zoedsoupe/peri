e16e510db646d3f1623c252b7279c598b73a5081
sitch <2736163+sitch@users.noreply.github.com>
2025-02-05 09:18:41 -0500

    Update .formatter.exs typo on `locals_without_parens` (#17)

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  export: [local_without_parens: [defschema: 2]]
+  export: [locals_without_parens: [defschema: 2]]
 ]
